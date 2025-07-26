95945d3241b4c4b7ab399728b0efc66bb982b155
Nguyen Hoang Long <longnh.uit@gmail.com>
2025-07-26 08:57:55 +0700

    feat: config and connect to database (postgresql)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"log"
 
-func main(){
-	fmt.Println("Hello Go")
+	"github.com/longnh462/go-gin-boilerplate/internal/configs"
+)
+
+func main() {
+	fmt.Println("Starting Go Gin Application.... 🚀🚀🚀🚀🚀")
+
+	// Connect to database
+	db, err := configs.ConnectDb()
+	if err != nil {
+		log.Fatalf("Failed to create db instance 🚫🚫🚫🚫🚫 %v", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Failed to create sqlDB instance 🚫🚫🚫🚫🚫 %v", err)
+	}
+	defer sqlDB.Close()
+
+	fmt.Println("Successfully connected to database 🆙🆙🆙🆙🆙")
 }
