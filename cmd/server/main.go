df21c4373ca50a46145345981d32cb0250b02d4c
Nguyen Hoang Long <longnh.uit@gmail.com>
2025-07-26 09:25:24 +0700

    feat: Test on gin route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/longnh462/go-gin-boilerplate/internal/configs"
 )
 
@@ -23,4 +25,23 @@ func main() {
 	defer sqlDB.Close()
 
 	fmt.Println("Successfully connected to database 🆙🆙🆙🆙🆙")
+
+	// Create Gin Router
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "Welcome to Go Gin Boilerplate API",
+		})
+	})
+
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "7070"
+	}
+
+	fmt.Printf("Server is running on %s: 🔥🔥🔥🔥🔥 \n", port)
+	if err := router.Run(":" + port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
