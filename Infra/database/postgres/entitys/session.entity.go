fcec80a3dd4860663a5ec30fc97eafb0aa6747b4
Nguyen Hoang Long <longnh.uit@gmail.com>
2025-07-30 23:00:05 +0700

    feat: enhance SessionEntity with JSON tags and GORM constraints

--- Infra/database/postgres/entitys/session.entity.go
+++ Infra/database/postgres/entitys/session.entity.go
@@ -4,14 +4,14 @@ import "time"
 
 type SessionEntity struct {
 	BaseEntity
-	SessionId  string
-	UserId     string
-	Token      string
-	ExpirestAt time.Time
-	LastActive time.Time
-	IPAddress  string
-	UserAgent  string
-	IsActive   bool
+	SessionId  string    `json:"sessionId" gorm:"primaryKey;uniqueIndex;not null;size:20"`
+	UserId     string    `json:"userId" gorm:"primaryKey;not null;index;size:20"`
+	Token      string    `json:"token" gorm:"not null;size:255"`
+	ExpirestAt time.Time `json:"expiresAt" gorm:"not null"`
+	LastActive time.Time `json:"lastActive" gorm:"not null"`
+	IPAddress  string    `json:"ipAddress" gorm:"not null;size:45"` // IPv4 or IPv6
+	UserAgent  string    `json:"userAgent" gorm:"not null;size:255"`
+	IsActive   bool      `json:"isActive" gorm:"default:false"`
 
-	User UserEntity
+	User UserEntity `json:"user,omitempty" gorm:"foreignKey:UserId;references:UserId"`
 }
