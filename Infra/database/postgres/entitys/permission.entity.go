ff1e800d8df963c4363986e5435e1a1eb5945f3e
Nguyen Hoang Long <longnh.uit@gmail.com>
2025-08-09 19:24:21 +0700

    feat: update database

--- Infra/database/postgres/entitys/permission.entity.go
+++ Infra/database/postgres/entitys/permission.entity.go
@@ -4,10 +4,10 @@ import "github.com/google/uuid"
 
 type PermissionEntity struct {
 	BaseEntity
-	PermissionId   uuid.UUID `json:"permission_id" gorm:"primaryKey;not null;size:36"`
-	PermissionName string    `json:"permission_name" gorm:"not null;size:255"`
-	Description    string    `json:"description" gorm:"size:500"`
-	IsActive       bool      `json:"is_active" gorm:"default:true"`
+	PermId   uuid.UUID `json:"permission_id" gorm:"primaryKey;not null;size:36"`
+	PermName string    `json:"permission_name" gorm:"not null;size:255"`
+	PerDesc  string    `json:"description" gorm:"size:500"`
+	IsActive bool      `json:"is_active" gorm:"default:true"`
 
 	// Relationships
 	Roles []RoleEntity `json:"roles,omitempty" gorm:"many2many:role_permissions;"`
