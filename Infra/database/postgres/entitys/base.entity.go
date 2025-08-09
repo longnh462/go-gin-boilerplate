4369d251ad46cab621a09b97be8ad9b2aa74a722
Nguyen Hoang Long <longnh.uit@gmail.com>
2025-08-09 14:44:04 +0700

    refactor: standardize formatting in entity files

--- Infra/database/postgres/entitys/base.entity.go
+++ Infra/database/postgres/entitys/base.entity.go
@@ -7,9 +7,9 @@ import (
 )
 
 type BaseEntity struct {
-	CreateAt    time.Time `json:"create_At" gorm:"autoCreateTime"`
-	UpdateAt    time.Time `json:"update_At" gorm:"autoCreateTime"`
-	CreateUsr   string    `json:"create_usr" gorm:"size:25;not null;default:'system'"`
-	UpdateUsr   string    `json:"update_usr" gorm:"size:25;not null;default:'system'"`
-	DeletedAt 	gorm.DeletedAt `gorm:"index"`
+	CreateAt  time.Time      `json:"create_At" gorm:"autoCreateTime"`
+	UpdateAt  time.Time      `json:"update_At" gorm:"autoCreateTime"`
+	CreateUsr string         `json:"create_usr" gorm:"size:25;not null;default:'system'"`
+	UpdateUsr string         `json:"update_usr" gorm:"size:25;not null;default:'system'"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
--- Infra/database/postgres/entitys/permission.entity.go
+++ Infra/database/postgres/entitys/permission.entity.go
@@ -5,9 +5,9 @@ import "github.com/google/uuid"
 type PermissionEntity struct {
 	BaseEntity
 	PermissionId   uuid.UUID `json:"permission_id" gorm:"primaryKey;not null;size:36"`
-	PermissionName string `json:"permission_name" gorm:"not null;size:255"`
-	Description    string `json:"description" gorm:"size:500"`
-	IsActive       bool   `json:"is_active" gorm:"default:true"`
+	PermissionName string    `json:"permission_name" gorm:"not null;size:255"`
+	Description    string    `json:"description" gorm:"size:500"`
+	IsActive       bool      `json:"is_active" gorm:"default:true"`
 
 	// Relationships
 	Roles []RoleEntity `json:"roles,omitempty" gorm:"many2many:role_permissions;"`
